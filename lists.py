4f0e25996978684d4b4c59c2b2212e3fbe81362f
FerCesio <162721896+FerCesio@users.noreply.github.com>
2024-05-10 12:29:48 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,20 +1,40 @@
-# Replace the "ANSWER HERE" with your answer
-
-def remove_elements(list_to_remove_elements):
-    return "ANSWER HERE"  # Remove this line and implement
-
-
-def add_elements(list_to_add_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+def remove_elements(list = ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']):
+    del list[0]
+    del list[(len(list)-1)]
+    del list[(len(list)-1)]
+    return list
+remove_elements()
 
 
-def is_empty(list_to_check):
-    return "ANSWER HERE"  # Remove this line and implement
+def add_elements(list = ["Red", "Green", "White", "Black"]):
+    list.insert(0, "Pink")
+    list.insert((len(list)), "Yellow")
+    return list
+add_elements()
+
+def is_empty(list = [1]):
+    empty = []
+    if list == empty:
+        return "Vacio"
+    else:
+        return "Tiene elementos"
+is_empty()
+
+def check_lists(list1 = ['Black', 'Pink', 'Yellow', 'Red', 'Green', 'White'], list2 = ['Red', 'Green', 'White', 'Black', 'Pink']):
+    if list1[2] == list2[2]:
+        return True
+    elif len(list1)<3 or len(list2)<3:
+        return False
+    else:
+        return False
+check_lists()
 
 
-def check_lists(list_to_compare1, list_to_compare2):
-    return "ANSWER HERE"  # Remove this line and implement
-
-
-def list_of_lists(list_of_lists_to_modify):
-    return "ANSWER HERE"  # Remove this line and implement
+def list_of_lists(list = [[1, 2, 3], [4, 5, 6, 7, 8], [9, 10, 11, 12]]):
+    a = list[0]
+    b = list[1]
+    c = list[2]
+    del b[0]
+    z=[a[:2], b[:3], c[-2:]]
+    return z
+list_of_lists()
